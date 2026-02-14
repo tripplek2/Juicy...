ad4105addc354fb057e7e6029c8c8383b8f94394
Kelvin Korir <kibetkelvin222@gmail.com>
2026-02-14 11:46:10 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Juice constructor
+
+function Juice(name, price, size, flavour) {
+    this.name = name;
+    this.price = price;
+    this.size = size;
+    this.flavour = flavour;
+    this.isAvailable = true;
+}
